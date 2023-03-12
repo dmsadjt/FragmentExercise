42c9fc7eb7feb071364bd50f63f5b149b5787f1e
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2023-03-12 11:42:52 +0700

    edit homefragment

--- app/src/main/java/com/example/fragmentexercise/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexercise/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.fragmentexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log
+        }
     }
 }
